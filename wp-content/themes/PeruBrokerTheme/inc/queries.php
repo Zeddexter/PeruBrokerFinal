8d511e6973cf19d0ee2acc08870d54ae826d2800
Joel Torres <dexterfx2012@gmail.com>
2019-09-24 09:50:28 -0500

    Se hizo commit

--- wp-content/themes/PeruBrokerTheme/inc/queries.php
+++ wp-content/themes/PeruBrokerTheme/inc/queries.php
@@ -174,7 +174,7 @@ function mostrar_servicios(){ ?>
                     // echo $image[0];
                     ?>" alt="">
                          <img src="<?php 
-                    the_field('imagen_3');
+                    echo the_field('imagen_4');
                     // $attachment_id = get_field('imagen_4');
                     // $size = "medium"; // (thumbnail, medium, large, full or custom size)
                     // $image = wp_get_attachment_image_src( $attachment_id, $size );
