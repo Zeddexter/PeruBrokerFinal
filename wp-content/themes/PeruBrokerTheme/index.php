2bf7d652ef71bff3ecd4c1eb418a3c8a9e0216b5
Joel Torres <dexterfx2012@gmail.com>
2019-10-16 12:27:30 -0500

    ultimas modificaciones en PB

--- wp-content/themes/PeruBrokerTheme/index.php
+++ wp-content/themes/PeruBrokerTheme/index.php
@@ -31,9 +31,9 @@
 <!-- FIN HERO --> 
 
 <?php get_template_part('template-parts/nosotros');?>
-<?php get_template_part('template-parts/staff');?>
+<?php //get_template_part('template-parts/staff');?>
 <?php get_template_part('template-parts/servicios');?>
-<?php get_template_part('template-parts/clientes');?>
+<?php //get_template_part('template-parts/clientes');?>
 <?php get_template_part('template-parts/reportes');?>
 
 
