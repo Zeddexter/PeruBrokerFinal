1942e8fe8d9c954abf353616865268d1ee49185c
Joel Torres <dexterfx2012@gmail.com>
2019-11-18 23:19:26 -0500

    download inline v0.0.2

--- wp-content/download_files.php
+++ wp-content/download_files.php
@@ -8,7 +8,9 @@ echo "Datos";
     header("Content-Type: application/pdf,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel");
     // header("Content-Disposition: attachment; filename=$saved_file_name");
     header("Content-Disposition: inline; filename=$saved_file_name");
+    header('Content-Transfer-Encoding: binary');
+    header('Accept-Ranges: bytes');
     header("Content-Length: " . filesize($actual_file_name));
-    readfile($actual_file_name);
+    @readfile($actual_file_name);
     exit;
 ?>
