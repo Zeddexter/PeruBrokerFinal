bfa36279c2fdf2a4fff219780823351e407923c6
Joel Torres <dexterfx2012@gmail.com>
2019-11-18 23:12:28 -0500

    download inline v0.0.1

--- wp-admin/download_files.php
+++ wp-admin/download_files.php
@@ -4,7 +4,8 @@
     $saved_file_name = $_GET['files'];
 //echo $actual_file_name." ".$saved_file_name;
     header("Content-Type: application/pdf,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel");
-    header("Content-Disposition: attachment; filename=$saved_file_name");
+    // header("Content-Disposition: attachment; filename=$saved_file_name");
+    header("Content-Disposition: inline; filename=$saved_file_name");
     header("Content-Length: " . filesize($actual_file_name));
     readfile($actual_file_name);
     exit;
