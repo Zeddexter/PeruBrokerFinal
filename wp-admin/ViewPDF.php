8b905a709f1b2d22ea9307e118ebcc12482229ad
Joel Torres <dexterfx2012@gmail.com>
2019-11-21 00:34:57 -0500

    version 0.0.6

--- wp-admin/ViewPDF.php
+++ wp-admin/ViewPDF.php
@@ -1,11 +1,9 @@
 <?php 
 
     $actual_file_name = $_GET['route_file'];
-    $saved_file_name = $_GET['files']; 
-    $uploadFileDir =  WP_CONTENT_DIR.'/uploaded_files/'.$saved_file_name;
-    ?>
-    <object data="<?php echo $uploadFileDir; ?>" type="application/pdf">
-    <embed src="<?php echo $uploadFileDir; ?>" type="application/pdf" />
+    $saved_file_name = $_GET['files']; ?>
+    <object data="<?php echo $actual_file_name; ?>" type="application/pdf">
+    <embed src="<?php echo $actual_file_name; ?>" type="application/pdf" />
 </object>
 <?php
 ?>
