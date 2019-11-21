edb292fcbdae31fe69ef11f0f347ed0c0935468c
Joel Torres <dexterfx2012@gmail.com>
2019-11-20 23:05:46 -0500

    version 0.0.2

--- wp-admin/download_files.php
+++ wp-admin/download_files.php
@@ -5,7 +5,7 @@
 //echo $actual_file_name." ".$saved_file_name;
     header("Content-Type: application/pdf,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel");
     // header("Content-Disposition: attachment; filename=$saved_file_name");
-    header("Content-Disposition: inline; filename=$actual_file_name");
+    header("Content-Disposition: inline; filename=$saved_file_name");
     header('Content-Transfer-Encoding: binary');
     header('Accept-Ranges: bytes');
     header("Content-Length: " . filesize($actual_file_name));
