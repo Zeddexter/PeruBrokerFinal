6d90b8de3dde7bd2be3eba206163063768ed3c9b
Joel Torres <dexterfx2012@gmail.com>
2019-12-03 09:53:04 -0500

    Footer

--- wp-content/themes/PeruBrokerTheme/footer.php
+++ wp-content/themes/PeruBrokerTheme/footer.php
@@ -2,14 +2,45 @@
 <footer>
         <div class="row">
             <div class="col">
-                    <div class="copy">
+                    <!-- <div class="copy">
                         <p>Copyright <?php echo date("Y") ?></p>
                     </div>
                     <div class="create">
                         <p>create by <a href="https://www.facebook.com/cyberzsoft/" target="_blan">CyberzSoft</a></p>
-                    </div>
+                    </div> -->
+                    <?php 
+            $args = array(
+                    'post_type' => 'broker_contacto',
+                    'posts_per_page' => 1,
+                    'orderby' => 'ID',
+                    'order' => 'DESC'
+            );
+           
+            $clases = new WP_Query($args);
+            while ($clases->have_posts() ): $clases -> the_post();
+            ?>
+            <div class="content-mapa">
+                <?php //the_content(); ?>
+            </div>
+            <div class="row">
+            <div class="col-md-6">
+                <div class="content-info">
+                    <p class="direccion" ><?php the_field('direccion');?></p>
+                    <p class="telefono"><?php the_field('telefono');?></p>
+                    <p class="correo"><?php the_field('correo');?></p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="content-info">
+                    <p class="fax"><?php the_field('fax');?></p>
+                    <p class="facebook"><a href="<?php the_field('link_facebook');?>"><?php the_field('nombre_facebook');?></a></p>
+                </div>
+            </div>
+            </div>
+        <?php endwhile; wp_reset_postdata(); ?> 
             </div>
         </div>
+        <br><br> <br>
     </footer>
     <?php wp_footer(); ?>
 </body>
