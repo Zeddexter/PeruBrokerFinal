cf022a753605243ddebfd91ea722b7e094044b82
Joel Torres <dexterfx2012@gmail.com>
2020-01-13 12:08:47 -0500

    Menu Reportes

--- wp-content/themes/PeruBrokerTheme/inc/opciones.php
+++ wp-content/themes/PeruBrokerTheme/inc/opciones.php
@@ -2,10 +2,10 @@
 <?php
 
 function perubroker_reportes(){
- add_menu_page('PeruBroker','Reportes','administrator','rp_estadisticas','rp_estadisticas','',20);
-add_submenu_page('rp_estadisticas','Todos los reportes','Todos los reportes','administrator','rp_estadisticas','rp_estadisticas');
-add_submenu_page('rp_estadisticas','Nuevo reporte','Nuevo reporte','administrator','rp_nuevos_registros','rp_nuevos_registros');
-add_submenu_page('rp_estadisticas','Tipos de reportes','Tipos de reportes','administrator','rp_tipo_reportes','rp_tipo_reportes');
+ add_menu_page('PeruBroker','Reportes','editor','rp_estadisticas','rp_estadisticas','',20);
+add_submenu_page('rp_estadisticas','Todos los reportes','Todos los reportes','editor','rp_estadisticas','rp_estadisticas');
+add_submenu_page('rp_estadisticas','Nuevo reporte','Nuevo reporte','editor','rp_nuevos_registros','rp_nuevos_registros');
+add_submenu_page('rp_estadisticas','Tipos de reportes','Tipos de reportes','editor','rp_tipo_reportes','rp_tipo_reportes');
 // add_submenu_page('rp_estadisticas','Modificar Tipo','Modificar Tipo','administrator','rp_edit_tipo_reportes','rp_edit_tipo_reportes');
 }
 
