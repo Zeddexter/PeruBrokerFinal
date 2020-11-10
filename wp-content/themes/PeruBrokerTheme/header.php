6b32ba00f9a235c6f7a3f326ce07bae182c0bc3d
jtorres <jtorres@SISTEMAS11>
2020-11-10 10:04:24 -0500

    A

--- wp-content/themes/PeruBrokerTheme/header.php
+++ wp-content/themes/PeruBrokerTheme/header.php
@@ -25,7 +25,7 @@
 <body>
 <!-- TOP - BARRA DE ARRIBA -->
    <header  id="inicio">
-  
+  <img src="<?php echo get_template_directory_uri(); ?>/img/Construccion.png"/>
        <div class="menu" id="menu">
            <div class="content-menu" >
 
