997ab9d2433e38838dee0257060c553f9fab6d14
DEXTERFX1 <dexterfx2012@gmail.com>
2021-01-01 23:01:12 -0500

    tipo3

--- wp-admin/Eliminatiporeporte.php
+++ wp-admin/Eliminatiporeporte.php
@@ -1,8 +1,10 @@
 <?php
 //echo $_SERVER['DOCUMENT_ROOT'];
 echo $_SERVER['DOCUMENT_ROOT'];
- require_once( $_SERVER['DOCUMENT_ROOT'] .'/perubrokerFinal/wp-config.php' );
- require_once( $_SERVER['DOCUMENT_ROOT'] . '/perubrokerFinal/wp-includes/wp-db.php' );
+// require get_template_directory().'/perubrokerFinal/wp-config.php';
+// require get_template_directory().'/perubrokerFinal/wp-includes/wp-db.php';
+require_once( $_SERVER['DOCUMENT_ROOT'] .'/wp-config.php' );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-includes/wp-db.php' );
 echo "<br>";
 echo $_GET["codigo"];
 
