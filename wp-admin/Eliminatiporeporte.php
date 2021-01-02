449984f3d06861628c86934662df18dcc43f86d2
DEXTERFX1 <dexterfx2012@gmail.com>
2021-01-01 22:25:11 -0500

    tipo4

--- wp-admin/Eliminatiporeporte.php
+++ wp-admin/Eliminatiporeporte.php
@@ -1,8 +1,8 @@
 <?php
 //echo $_SERVER['DOCUMENT_ROOT'];
 echo $_SERVER['DOCUMENT_ROOT'];
-// require_once( $_SERVER['DOCUMENT_ROOT'] .'/perubrokerFinal/wp-config.php' );
-// require_once( $_SERVER['DOCUMENT_ROOT'] . '/perubrokerFinal/wp-includes/wp-db.php' );
+ require_once( $_SERVER['DOCUMENT_ROOT'] .'/perubrokerFinal/wp-config.php' );
+ require_once( $_SERVER['DOCUMENT_ROOT'] . '/perubrokerFinal/wp-includes/wp-db.php' );
 echo "<br>";
 echo $_GET["codigo"];
 
@@ -14,17 +14,23 @@ if(isset($_GET["codigo"]))
     echo $id;
     global $wpdb;
     // $wpdb->delete('wp_tipo_reportes',array('id'=>$id));
-    
-    $table = $wpdb->prefix.'tipo_reportes';
 
-        $wpdb->show_errors(); 
-        $wpdb->delete( $table, array('id' => $id) );
-        $wpdb->print_error();
+    $table_name = 'wp_tipo_reportes';
+    if (!empty($id)) 
+    {
+        $wpdb->delete('wp_tipo_reportes', array('id' => $id) );
+     // $wpdb->query($wpdb->prepare("DELETE FROM $table_name WHERE id IN($id)"));
+    
+    }
+
+    //    // $wpdb->show_errors(); 
+    //     $wpdb->delete( 'wp_tipo_reportes', array('id' => $id) );
+       // $wpdb->print_error();
         echo "<br>final";
 
 }
-wp_redirect(admin_url(esc_url(home_url( '/' ))."wp-admin/admin.php?page=rp_tipo_reportes"));   
-//header("Location: ".esc_url(home_url( '/' ))."wp-admin/admin.php?page=rp_tipo_reportes");
+//wp_redirect(admin_url(esc_url(home_url( '/' ))."wp-admin/admin.php?page=rp_tipo_reportes"));   
+header("Location: ".esc_url(home_url( '/' ))."wp-admin/admin.php?page=rp_tipo_reportes");
 //Connect DB
 //Create query based on the ID passed from you table
 //query : delete where Staff_id = $id
